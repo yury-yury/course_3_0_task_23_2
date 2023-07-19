eca8d37a276a3b893cd337f00cb3065e9216310b
yury-yury <yury_yury@mail.ru>
2023-07-19 23:44:29 +0300

    add pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.views import contact, main
 
 urlpatterns = [
-    # path('signup', UserCreateView.as_view()),
+    path('', main),
+    path('contact', contact),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    if request.method == 'GET':
+        return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        visiter = dict()
+        visiter['name'] = request.POST.get('name', None)
+        visiter['phone'] = request.POST.get('phone', None)
+        visiter['message'] = request.POST.get('message', None)
+        print(visiter)
+    return render(request, 'catalog/contact.html', )
