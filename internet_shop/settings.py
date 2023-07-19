229f2c88c55863894d4cca4eb056813ffc62afa7
yury-yury <yury_yury@mail.ru>
2023-07-19 14:42:28 +0300

    connected the database

--- internet_shop/settings.py
+++ internet_shop/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$y6q!zolryq3y+w#ve%ayu2a0pd%iv=2w7%$_ra@&yc1@*)i5'
+SECRET_KEY = os.environ.get("SECRET_KEY") # 'django-insecure-3$y6q!zolryq3y+w#ve%ayu2a0pd%iv=2w7%$_ra@&yc1@*)i5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +82,14 @@ WSGI_APPLICATION = 'internet_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE", 'django.db.backends.postgresql'),
+        'NAME': os.environ.get("DB_NAME", 'todolist'),
+        'USER': os.environ.get("DB_USER", 'postgres'),
+        'PASSWORD': os.environ.get("DB_PASSWORD", 'postgres'),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
