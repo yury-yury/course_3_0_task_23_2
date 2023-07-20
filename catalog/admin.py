ddcbc834ce830a610d37247d98fa332fe0d78df8
yury-yury <yury_yury@mail.ru>
2023-07-20 15:41:51 +0300

    create admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
+from typing import Tuple
+
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    The CategoryAdmin class inherits from the ModelAdmin class. Defines the output of instance fields
+    to the administration panel and the ability to edit them.
+    """
+    list_display: Tuple[str, ...] = ("id", "name")
+    list_display_links: Tuple[str, ...] = ("id", "name")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    The ProductAdmin class inherits from the ModelAdmin class. Defines the output of instance fields
+    to the administration panel and the ability to edit them.
+    """
+    list_display: Tuple[str, ...] = ("id", "name", "price", "category")
+    list_display_links: Tuple[str, ...] = ("id", "name")
+    readonly_fields: Tuple[str, ...] = ("id", "created_at", "updated_at")
+    search_fields: Tuple[str, ...] = ("name", "description")
+    list_filter: Tuple[str, ...] = ("category", )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -4,17 +4,31 @@ from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    class Meta:
+        """
+        The Meta class contains the common name of the model instance in the singular and plural used
+        in the administration panel.
+        """
+        verbose_name: str = "Категория"
+        verbose_name_plural: str = "Категории"
 
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='product')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(decimal_places=2, max_digits=12)
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего обновления")
+
+    def __str__(self) -> str:
+        return f"{self.name}"
 
     def save(self, *args, **kwargs):
         """
@@ -25,3 +39,11 @@ class Product(models.Model):
             self.created = timezone.now()
         self.updated = timezone.now()
         return super().save(*args, **kwargs)
+
+    class Meta:
+        """
+        The Meta class contains the common name of the model instance in the singular and plural used
+        in the administration panel.
+        """
+        verbose_name: str = "Продукт"
+        verbose_name_plural: str = "Продукты"
